22189949a0354579f5db801f9a5f211853dd75fb
dennysanthoni <154483686+dennysanthoni@users.noreply.github.com>
2024-02-02 19:11:13 -0500

    Create main.cpp

---
+++ El perrito que quiere un hueso corregido/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int oh1,oh2,th1,th2;
+    cin>>oh1>>th1>>oh2>>th2;
+    if(oh1>oh2&&th1>th2){
+        cout<<"Hueso 1";
+    }else if (oh1<oh2&&th1<th2){
+        cout<<"Hueso 2";
+    }else{cout<<"Perrito confundido :(";}
+   return 0;
+}
