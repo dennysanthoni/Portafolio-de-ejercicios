9497829d5ae047de44554e1747b235ff54b0c91b
dennysanthoni <154483686+dennysanthoni@users.noreply.github.com>
2024-02-04 19:20:57 -0500

    Create main.cpp

---
+++ El excursionista Mayor/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,edad,i,mayor=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        scanf("%d",&edad);
+        if(edad>mayor) mayor=edad;
+    }
+    printf("%d\n",mayor);
+
+    return 0;
+}
